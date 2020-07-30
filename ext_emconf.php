6d4a9592facdb1cd2afce31efcf7a19b48eefb94
Kerstin Schmitt <kerstin.schmitt@wacon.de>
2020-07-30 09:53:27 +0200

    Update core-requirements
    Update core-requirements

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.5',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-9.5.99',
+            'typo3' => '9.5.20-9.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
